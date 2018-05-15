f1d9b7d9e3366d168efe39468951a22b1017d5e3
diego crespo <crespodi@bu.edu>
2018-05-15 18:52:09 -0400

    did some excersises, added some folders, took some notes

---
+++ chapter_1/ex_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// short int -32,768 to 32,767
+// unsigned short int 0 to 65,535
+// int -2,147,483,658 2,147,483,657
+// unsigned int 0 to 4,294,967,295
+// long int -2,147,483,648 to 2,147,483,647
+// unsigned long int
+// float 3.4e +/- 38(7 sig figs)
+// double 1.7E +/- 308 (15 sig figs)
+// long double 1.7E +/- 308 (15 sig figs)
+// char 256 character values
+// bool true or false
+
+// Identifier only contains #'s letters and underscores
+// Identifier shouldn't use keywords
+// Identifier should start with numbers
+
+int main()
+{
+  int var1;
+  string name;
+  unsigned int var2;
+
+  double 1badvar;
+  char char;
+  bool true;
+
+}
---
+++ chapter_1/ex_2.cpp
@@ -0,0 +1,14 @@
+// What is displayed by each line in the following code snippet?
+
+// cout << "Seven divided by three is " << 7 / 3 << endl; 2
+// cout << "Seven divided by three is " << 7.0 / 3.0 << endl; 2.3333
+// cout << "Seven divided by three is " << 7.0 / 3 << endl; 2.3333
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Seven divided by three is " << 7.0 / 3 << endl;
+}
